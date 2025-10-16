951508331784e37631f9db5de18e02cace5803da
Chedy soltani <chedysoltani0@gmail.com>
2025-10-16 19:38:17 +0100

    auth

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../../prisma/prisma.service';
+import { SignupDto, LoginDto } from './dto/auth.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async signup(dto: SignupDto) {
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+    const user = await this.prisma.user.create({
+      data: { ...dto, password: hashedPassword },
+    });
+    return this.signToken(user.id, user.email, user.role);
+  }
+
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findUnique({ where: { email: dto.email } });
+    if (!user) throw new UnauthorizedException('Email non trouvé');
+
+    const passwordMatches = await bcrypt.compare(dto.password, user.password);
+    if (!passwordMatches) throw new UnauthorizedException('Mot de passe incorrect');
+
+    return this.signToken(user.id, user.email, user.role);
+  }
+
+  private async signToken(userId: number, email: string, role: string) {
+    const payload = { sub: userId, email, role };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
